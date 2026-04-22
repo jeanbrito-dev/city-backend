9db11b84621b62dbf69e84b00aff7aa43f5c7c55
Brito <jean.gustavo078@gmail.com>
2026-04-22 11:26:06 -0300

    add mock api for login and register

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import occurrenceRoutes from "./routes/occurrenceRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
 
 const app = express();
 
@@ -8,6 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/occurrences", occurrenceRoutes);
+app.use("/auth", authRoutes);
 
 app.listen(3000, () => {
   console.log("Servidor rodando em http://localhost:3000");
