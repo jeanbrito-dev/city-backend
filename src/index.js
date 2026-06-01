9c11e7a44fade26eda0e34599237eeb015196ffe
andradejvc <joaovictorcamargoandrade@gmail.com>
2026-06-01 17:15:56 -0300

    fix: use railway port

--- src/index.js
+++ src/index.js
@@ -18,6 +18,8 @@ app.use("/auth", authRoutes);
 app.use("/geocode", geocodeRoutes);
 app.use("/comments", commentRoutes);
 
-app.listen(3000, () => {
-  console.log("Servidor rodando em http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
