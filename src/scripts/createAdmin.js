d73522aaeba159ec21320cfd523fec6a62711d6d
JEAN GUSTAVO BRITO DIAS <jean.gustavo078@gmail.com>
2026-06-03 15:33:22 -0300

    fix(admin): instalado bcript

--- src/scripts/createAdmin.js
+++ src/scripts/createAdmin.js
@@ -1,5 +1,5 @@
 import bcrypt from "bcrypt";
-import prisma from "../lib/prisma.js";
+import { prisma } from "../lib/prisma.js";
 
 async function main() {
   const hash = await bcrypt.hash("Mega.admin67", 10);
